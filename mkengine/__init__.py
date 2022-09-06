68421d2977dc634de78344e2ee7f8cb01d6dac2f
Melkor <rintfd@163.com>
2022-09-07 00:30:07 +0800

    udpate

--- mkengine/__init__.py
+++
@@ -1,4 +0,0 @@
-from .neuro_concept_structure import *
-from .neuro_config import *
-from .neuro_vertex import *
-from .neuro_types import *
--- mkengine/neuro_config.py
+++
@@ -1,4 +0,0 @@
-# This is the default quasi engine configuration
-OBJECT_FEATURE_DIM = 256
-CONCEPT_FEATURE_DIM = 256
-RELATION_FEATURE_DIM = 512
