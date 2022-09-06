9e4129a3f67e7904b6403e0fe0f641aff6af1774
Melkor <rintfd@163.com>
2022-09-07 00:31:35 +0800

    update

--- mk_engine/__init__.py
+++
@@ -1,3 +0,0 @@
-from aluneth.rinlearn.ns.neuro_engine.concept_structure import *
-from aluneth.rinlearn.ns.neuro_engine.primitives import *
-from aluneth.rinlearn.ns.neuro_engine.executor import *
---
+++ mkengine/__init__.py
@@ -0,0 +1,4 @@
+from .neuro_concept_structure import *
+from .neuro_config import *
+from .neuro_vertex import *
+from .neuro_types import *
